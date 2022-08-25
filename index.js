fd3eb8b576630e77aa2a76192ee8ccaeb6683482
Alexandra <alexandraneff@live.com>
2022-08-24 20:58:09 -0500

    commit tree 7acc11f00432d8dc44d2b1add3eb35471e8d3d04

---
+++ index.js
@@ -0,0 +1,5 @@
+function divide(parameter1, parameter2) {
+    return parameter1 / parameter2
+}
+const square = (paramater1) => paramater1 **2 
+const add = (parameter1, paramater2) => parameter1 + paramater2
